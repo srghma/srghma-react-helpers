351686ee095ce91894b427cee4b9fbdbb1453b90
Serhii Khoma <srghma@gmail.com>
2020-03-09 20:26:06 +0200

    Update createReducer.js

--- src/createReducer.js
+++ src/createReducer.js
@@ -1,3 +1,20 @@
+// Example usage:
+//
+// const scope = 'PAGE1_PAGE'
+// const scopedFactory = actionCreatorFactory(scope)
+//
+// const incrementAction = scopedFactory('INCREMENT')
+//
+// const initialState = 0
+//
+// const actionsMap = {
+//   [incrementAction.type]: (state, payload, action) => state + 1
+// }
+//
+// const rootReducer = {
+//   page1: createReducer(page1ActionsMap, page1InitialState)
+// }
+
 function createReducer(declaration, initialValue) {
   return function reducerFn(state = initialValue, action) {
     const reducer = declaration[action.type]
